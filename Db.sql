3a3e2372dbdbd93115316517534569dc10546c09
siva8091 <46103816+siva8091@users.noreply.github.com>
2025-11-11 17:59:06 +0530

    Update Db.sql

--- Db.sql
+++ Db.sql
@@ -14,3 +14,14 @@ SELECT datname, temp_bytes
 FROM pg_stat_database
 ORDER BY temp_bytes DESC;
 
+SELECT uuid, status, task_type, created_at
+FROM ce_activity
+WHERE status IN ('PENDING', 'IN_PROGRESS', 'FAILED', 'CANCELED')
+ORDER BY created_at DESC;
+
+DELETE FROM ce_queue;
+DELETE FROM ce_activity WHERE status IN ('FAILED', 'CANCELED');
+
+SELECT NOW();
+
+
