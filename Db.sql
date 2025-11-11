1e0e58dc57c64d9dc2dec2823bbd71cd08a1d490
siva8091 <46103816+siva8091@users.noreply.github.com>
2025-11-11 17:43:10 +0530

    Update Db.sql

--- Db.sql
+++ Db.sql
@@ -2,3 +2,15 @@ SELECT client_addr, application_name, count(*)
 FROM pg_stat_activity 
 WHERE datname = 'sonarqube'
 GROUP BY client_addr, application_name;
+
+
+SHOW shared_buffers;
+SHOW work_mem;
+SHOW maintenance_work_mem;
+SHOW effective_cache_size;
+SHOW temp_buffers;
+
+SELECT datname, temp_bytes
+FROM pg_stat_database
+ORDER BY temp_bytes DESC;
+
