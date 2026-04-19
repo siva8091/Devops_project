db0b065366f1b796066bcdf45638655a3557fac3
siva8091 <46103816+siva8091@users.noreply.github.com>
2026-04-19 14:22:37 +0530

    Update Paramter.groovy

--- Paramter.groovy
+++ Paramter.groovy
@@ -3,3 +3,10 @@ WORKSPACE = os.environ.get("WORKSPACE", ".")
 INPUT_FILE = os.environ.get("INPUT_FILE", os.path.join(WORKSPACE, "jenkins_jobs.txt"))
 OUTPUT_CSV = os.environ.get("OUTPUT_CSV", os.path.join(WORKSPACE, "output.csv"))
 PROGRESS_FILE = os.path.join(WORKSPACE, "progress.json")
+
+WORKSPACE = os.environ.get("WORKSPACE", ".")
+
+INPUT_FILE = os.environ.get("INPUT_FILE")
+OUTPUT_CSV = os.path.join(WORKSPACE, os.environ.get("OUTPUT_CSV", "output.csv"))
+PROGRESS_FILE = os.path.join(WORKSPACE, "progress.json")
+
